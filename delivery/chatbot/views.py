9b0db32065c3cb68e5be351347825d5597ef1133
mayconspirlandelli <mayconbatista8@gmail.com>
2025-01-27 20:39:25 -0300

    CConstruindo chatbot

--- delivery/chatbot/views.py
+++ delivery/chatbot/views.py
@@ -1,51 +1,45 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-# from .models import Chat
-#import openai
 from django.utils import timezone
 from dotenv import load_dotenv
 import os
 from groq import Groq
 from llama_index.llms.groq import Groq
 
+
+# def teste():
+#     import os
+#     from groq import Groq
+
+#     client = Groq(
+#         api_key=os.environ.get("GROQ_API_KEY"),
+#     )
+
+#     chat_completion = client.chat.completions.create(
+#         messages=[
+#             {
+#                 "role": "user",
+#                 "content": "Explain the importance of fast language models",
+#             }
+#         ],
+#         model="llama-3.3-70b-versatile",
+#     )
+
+#     print(chat_completion.choices[0].message.content)
+
+
 def ask_groq(message):
-    
+    #Carrega chave
+    load_dotenv()
+
     # Create the Groq client
-    client = Groq(api_key=os.environ.get("GROQ_API_KEY"), )
+    GROQ_API = os.environ.get("GROQ_API_KEY")
 
-    # Set the system prompt
-    system_prompt = {
-        "role": "system",
-        "content":
-        "You are a helpful assistant. You reply with very short answers."
-    }
+    llm = Groq(model='llama3-70b-8192', api_key=GROQ_API)
 
-    # Initialize the chat history
-    chat_history = [system_prompt]
-
-    while True:
-    # Get user input from the console
-    user_input = input("You: ")
-
-    # Append the user input to the chat history
-    chat_history.append({"role": "user", "content": user_input})
-
-    response = client.chat.completions.create(model="llama3-70b-8192",
-                                                messages=chat_history,
-                                                max_tokens=100,
-                                                temperature=1.2)
-    # Append the response to the chat history
-    chat_history.append({
-        "role": "assistant",
-        "content": response.choices[0].message.content
-    })
-    # Print the response
-    print("Assistant:", response.choices[0].message.content)
-
-
-
-
-
+    #response = llm.complete('Qual é a substância que dá o aroma do alecrim?')
+    response = llm.complete('Qual a diferença entre llamaindex, llama e groq')
+    return response
 
 
 # def ask_openai(message):
@@ -60,26 +54,23 @@ def ask_groq(message):
 #     answer = response.choices[0].message.content.strip()
 #     return answer
 
+
 # Create your views here.
-# def chatbot(request):
-#     chats = Chat.objects.filter(user=request.user)
-
-#     if request.method == 'POST':
-#         message = request.POST.get('message')
-#         response = ask_openai(message)
-
-#         chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
-#         chat.save()
-#         return JsonResponse({'message': message, 'response': response})
-#     return render(request, 'chatbot.html', {'chats': chats})
-
-
-def chatbot (request):
+def chatbot(request):
     if request.method == 'POST':
-        message = request.POST.get ('message')
-        response = 'Hi this is my response'
+        message = request.POST.get('message')
+        response = ask_groq(message)
+
         return JsonResponse({'message': message, 'response': response})
-    return render (request, "chatbot.html")
+    return render(request, 'chatbot.html')
+
+
+# def chatbot (request):
+#     if request.method == 'POST':
+#         message = request.POST.get ('message')
+#         response = 'Hi this is my response'
+#         return JsonResponse({'message': message, 'response': response})
+#     return render (request, "chatbot.html")
 
 
 
